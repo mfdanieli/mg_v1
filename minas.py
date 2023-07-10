34b9c575a39ea0fb588ff44f5d04dc9296bb8870
Danieli Ferreira <97743505+mfdanieli@users.noreply.github.com>
2023-07-10 14:21:50 -0300

    Update minas.py

--- minas.py
+++ minas.py
@@ -47,6 +47,9 @@ st.set_page_config(page_title='Water quality',page_icon='💦', layout='wide')
 
 df = pd.read_excel('df_aux1.xlsx').drop('Unnamed: 0',axis=1)
 
+numeric_features = df.select_dtypes(include=['int', 'float']).columns
+df[numeric_features] = df[numeric_features].astype(float)
+
 # st.header('Modeling Fe and Mn during the dry and rainy')
 
 # criar abas
