2689401599de59e2fc27a5657406b784c01e35ac
Danieli Ferreira <97743505+mfdanieli@users.noreply.github.com>
2023-07-10 18:10:07 -0300

    Update minas.py

--- minas.py
+++ minas.py
@@ -161,23 +161,23 @@ def tratamento_encoding(X):
     le3 = LabelEncoder()
     df_tratado['curso_dágua'] = le3.fit_transform(df_tratado['curso_dágua'])
     
-     # day of week
-    df_tratado['year_sin'] = df_tratado['year'].apply( lambda x: np.sin( x * ( 2. * np.pi/7 ) ) )
-    df_tratado['year_cos'] = df_tratado['year'].apply( lambda x: np.cos( x * ( 2. * np.pi/7 ) ) )
+    #  # day of week
+    # df_tratado['year_sin'] = df_tratado['year'].apply( lambda x: np.sin( x * ( 2. * np.pi/7 ) ) )
+    # df_tratado['year_cos'] = df_tratado['year'].apply( lambda x: np.cos( x * ( 2. * np.pi/7 ) ) )
 
-    # month
-    df_tratado['month_sin'] = df_tratado['month'].apply( lambda x: np.sin( x * ( 2. * np.pi/12 ) ) )
-    df_tratado['month_cos'] = df_tratado['month'].apply( lambda x: np.cos( x * ( 2. * np.pi/12 ) ) )
+    # # month
+    # df_tratado['month_sin'] = df_tratado['month'].apply( lambda x: np.sin( x * ( 2. * np.pi/12 ) ) )
+    # df_tratado['month_cos'] = df_tratado['month'].apply( lambda x: np.cos( x * ( 2. * np.pi/12 ) ) )
 
-    # day 
-    df_tratado['day_sin'] = df_tratado['day'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
-    df_tratado['day_cos'] = df_tratado['day'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
+    # # day 
+    # df_tratado['day_sin'] = df_tratado['day'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
+    # df_tratado['day_cos'] = df_tratado['day'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
     
-    # others
-    df_tratado['day_of_week_sin'] = df_tratado['day_of_week'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
-    df_tratado['day_of_week_cos'] = df_tratado['day_of_week'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
-    df_tratado['week_of_year_sin'] = df_tratado['week_of_year'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
-    df_tratado['week_of_year_cos'] = df_tratado['week_of_year'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
+    # # others
+    # df_tratado['day_of_week_sin'] = df_tratado['day_of_week'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
+    # df_tratado['day_of_week_cos'] = df_tratado['day_of_week'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
+    # df_tratado['week_of_year_sin'] = df_tratado['week_of_year'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
+    # df_tratado['week_of_year_cos'] = df_tratado['week_of_year'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
 
 
     return df_tratado
